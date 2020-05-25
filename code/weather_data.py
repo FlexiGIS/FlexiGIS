86c32e32814991935195d53e1d4398425931f015
chunai <uniachi.chinonso@gmail.com>
2020-05-25 10:46:36 +0200

    modify feedin integration code

--- code/weather_data.py
+++ code/weather_data.py
@@ -1,14 +1,14 @@
-"""Get ERA5 weather data from CDS database using feedinlib module format.
+"""Get ERA5 weather data from CDS database using feedinlib interface.
 
 CDS: see https://cds.climate.copernicus.eu/#!/home
 This script gets feedinlib input parameters from the config.mk file.
 These parameters can be changed depending on the user interest.
 
-target_file:=ERA5_data.nc : Netcdf file of weather data downloaded from CDS using feedinlib module
-start_date:=2018-01-01 : Start date of dowloaded  weather data (default time in UTC)
-end_date:= 2018-12-31 : End date of downloaded weather data (default  time in UTC)
-lon:=8.15 : Longitude of data point
-lat:=53.20 : Latitutude of data point
+target_file  : Netcdf file of weather data downloaded from CDS using feedinlib module
+start_date  : Start date of dowloaded  weather data (default time in UTC)
+end_date : End date of downloaded weather data (default  time in UTC)
+lon  : Longitude of data point (float / list)
+lat : Latitutude of data point (float / list)
 
 see https://github.com/oemof/feedinlib/tree/dev/example for an example implementation of feedinlib.
 """
@@ -19,9 +19,16 @@ import sys
 weather_dir = "../data/01_raw_input_data/"
 
 
-def get_data(start_date, end_date, lat, lon, target_file):
-    """Get meteorological data from CDS usinf feedlib.era5 module."""
-    print("This may take some time to finish downloading")
+def get_data(start_date, end_date, lat, lon, target_file, region=True):
+    """Get meteorological data from CDS using feedlib.era5 interface."""
+    print(" This may take some time to finish downloading")
+    if region is True:
+        lon = [float(x) for x in list(lon.split(","))]
+        lat = [float(x) for x in list(lat.split(","))]
+    else:
+        lon = float(lon)
+        lat = float(lat)
+
     era5.\
         get_era5_data_from_datespan_and_position(variable="feedinlib",
                                                  start_date=start_date,
@@ -33,37 +40,10 @@ def get_data(start_date, end_date, lat, lon, target_file):
     print(data_meta)
 
 
-def feedin_solarFormat(lon, lat, target_file, to_csv=False):
-    """Get weather data in pvlib format."""
-    pv_data = era5.weather_df_from_era5(era5_netcdf_filename=target_file,
-                                        lib="pvlib",
-                                        area=[lon, lat])
-    print(pv_data.head(5))
-
-    if to_csv:
-
-        # export to csv file
-        pv_data.to_csv(weather_dir+"solar_data.csv")
-    # return pv_data
-
-
-def feedin_windFormat(lon, lat, target_file, to_csv=False):
-    """Get weather data in windpowerlib format."""
-    wind_data = era5.weather_df_from_era5(era5_netcdf_filename=target_file,
-                                          lib="windpowerlib",
-                                          area=[lon, lat])
-    print(wind_data.head(5))
-
-    if to_csv:
-        wind_data.to_csv(weather_dir+"wind_data.csv")
-    # return wind_data
-
-
 if __name__ == "__main__":
-    lon = float(sys.argv[1])
-    lat = float(sys.argv[2])
+    lon = sys.argv[1]
+    lat = sys.argv[2]
     target_file = sys.argv[3]
     start_date, end_date = sys.argv[4], sys.argv[5]
-    # get_data(start_date, end_date, lat, lon, target_file)
-    feedin_solarFormat(lon, lat, target_file, to_csv=True)
-    feedin_windFormat(lon, lat, target_file, to_csv=True)
+    region = sys.argv[6]
+    get_data(start_date, end_date, lat, lon, target_file, region=region)
